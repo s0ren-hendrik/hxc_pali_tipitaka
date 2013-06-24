35ed8f92810d98f5455c4fb52c1cff63bdaf1366
Nicolas Alfonsi <n.alfonsi@mental-orb.com>
2013-06-24 17:49:31 +0200

    cleaning cloacking headers (title + desc) for no-ajax crawling bots (soon deprecated)

--- infested_header.php
+++ infested_header.php
@@ -16,28 +16,6 @@ if(eregi('home', $inf))
 {
 	echo '<title>'.MAIN_TITLE.'</title>
 	<meta name="description" content="'.MAIN_DESC.'" />';
-}elseif(eregi('actus', $inf))
-{
-	$newsId = $loadThisPage[3];
-	
-	$rst = mysql_query("SELECT COUNT(id) FROM actus WHERE lnk='$newsId';");
-	$nb = mysql_num_rows($rst);
-	if($nb==1)
-	{
-		$ntitle = mysql_fetch_object(mysql_query("SELECT title FROM actus WHERE lnk='$newsId'"));
-		echo '<title>'.$ntitle->title." - ".MAIN_TITLE.'</title>';
-	}else
-	{
-		echo '<title>'.MAIN_TITLE.'</title>';
-	}
-	
-	echo '
-	<meta name="description" content="'.MAIN_DESC.'" />';
-}elseif($ltp == 'football')
-{
-	echo '<title>Les bases du Football Américain - '.MAIN_TITLE.'</title>';
-	echo '
-	<meta name="description" content="'.MAIN_DESC.'" />';
 }else
 {
 	echo '<title>'.MAIN_TITLE.'</title>
